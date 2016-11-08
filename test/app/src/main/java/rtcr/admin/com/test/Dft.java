c3dc0d321cd6c1b96d2dcdc87a4e10fc1eef9b9f
cyrilmathew13 <cyril.mathew13@vit.edu>
2016-11-08 17:11:39 +0530

    cos

--- test/app/src/main/java/rtcr/admin/com/test/Dft.java
+++ test/app/src/main/java/rtcr/admin/com/test/Dft.java
@@ -5,7 +5,9 @@ import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 /**
@@ -15,10 +17,14 @@ public class Dft {
     private Context mContext;
     int count=0;
     StringBuilder s1=new StringBuilder(1000);
+    StringBuilder s2=new StringBuilder(1000);
     String complex;
-
-    public void normalize(Context context,String pixel[],int pixelCount)
+    int sampleNo;
+    Context context1;
+    public void normalize(Context context,String pixel[],int pixelCount,int aaa)
     {
+        context1=context;
+        sampleNo=aaa;
         int ii=0,jj;
         double sumX,sumY,n;
 
@@ -110,7 +116,7 @@ public class Dft {
             int ii=0;
             int jj=1;
           //  while ((line = reader.readLine()) != null) {
-            while(ii<pixelCount)
+            while(ii<16)
             {
                 X= new double[1000];
                 Y= new double[1000];
@@ -120,7 +126,7 @@ public class Dft {
                 line=pixel[ii];
                 String[] temp = (line.split(","));
                 jj=ii;
-                while(jj<pixelCount)
+                while(jj<16)
                 {
                     String temp1[]=pixel[jj].split(",");
                     if(temp[2].equals(temp1[2]))
@@ -202,10 +208,173 @@ public class Dft {
             s1.append(complex);
             s1.append("\n");
         }
-        s1.append("********************\n");
+     //   s1.append("********************\n");
+        cos();
     }
 
+    void cos()
+    {
+        double sample[][]= new double[1000][2];
 
+        try {
+            BufferedReader br=new BufferedReader(new FileReader(new File(Environment.getExternalStorageDirectory(),"dft.txt")));
+            double sample1[][]=new double[1000][2];
+            double sample2[][]=new double[1000][2];
+            double sample3[][]=new double[1000][2];
+            double sample4[][]=new double[1000][2];
+            double sample5[][]=new double[1000][2];
+            int i=0,j=0;
+            String ss;
+            String ll[];
+            while((ss=br.readLine())!=null)
+            {
+                Log.d("read:","no");
+                if(!ss.contains("=="))
+                {
+                    switch(i)
+                    {
+                        case 1:
+                            ll=ss.split(",");
+                            sample1[j][0]=Double.parseDouble(ll[0].substring(1));
+                            sample1[j][1]=Double.parseDouble(ll[1].substring(1));
+                            break;
+
+                        case 2:
+                            ll=ss.split(",");
+                            sample2[j][0]=Double.parseDouble(ll[0].substring(1));
+                            sample2[j][1]=Double.parseDouble(ll[1].substring(1));
+                            break;
+
+                        case 3:
+                            ll=ss.split(",");
+                            sample3[j][0]=Double.parseDouble(ll[0]);
+                            sample3[j][1]=Double.parseDouble(ll[1]);
+                            break;
+
+                        case 4:
+                            ll=ss.split(",");
+                            sample4[j][0]=Double.parseDouble(ll[0]);
+                            sample4[j][1]=Double.parseDouble(ll[1]);
+                            break;
+
+                        case 5:
+                            ll=ss.split(",");
+                            sample5[j][0]=Double.parseDouble(ll[0]);
+                            sample5[j][1]=Double.parseDouble(ll[1]);
+                            break;
+                    }
+                    j++;
+                }
+                else
+                {
+                    i++;
+                    j=0;
+                }
+
+
+            }
+            if(sampleNo>0)
+            {
+                double cc;
+                switch(sampleNo)
+                {
+                    case 1:
+                        cc=cosine(sample1,sample2);
+                        s2.append(cc);
+                        s2.append("************");
+                        break;
+
+                    case 2:
+                        cc=cosine(sample1,sample3);
+                        s2.append(cc);
+                        cc=cosine(sample3,sample2);
+                        s2.append(cc);
+                        s2.append("************");
+                        break;
+
+                    case 3:
+                        cc=cosine(sample4,sample2);
+                        s2.append(cc);
+                        cc=cosine(sample4,sample3);
+                        s2.append(cc);
+                        cc=cosine(sample1,sample4);
+                        s2.append(cc);
+                        s2.append("************");
+                        break;
+
+                    case 4:
+                        cc=cosine(sample5,sample2);
+                        s2.append(cc);
+                        cc=cosine(sample5,sample3);
+                        s2.append(cc);
+                        cc=cosine(sample1,sample5);
+                        s2.append(cc);
+                        cc=cosine(sample4,sample5);
+                        s2.append(cc);
+                        s2.append("************");
+                        break;
+                }
+
+                cosWrite();
+            }
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    double cosine(double a[][],double b[][])
+    {
+        double c;
+        int i=0;
+        double ss=0,ss1=0,ss2=0;
+        while(i<16)
+        {
+            ss=a[i][0]*b[i][0]+a[i][1]*b[i][1]+ss;
+            ss1=a[i][0]*a[i][0]+a[i][1]*a[i][1]+ss1;
+            ss2=b[i][0]*b[i][0]+b[i][1]*b[i][1]+ss2;
+            i++;
+        }
+        ss1=Math.sqrt(ss1);
+        ss2=Math.sqrt(ss2);
+
+        c=ss/(ss1*ss2);
+        Log.d("cos:",String.valueOf(c)+","+String.valueOf(ss1)+","+String.valueOf(ss2)+","+String.valueOf(ss));
+
+        return c;
+
+    }
+
+    void cosWrite()
+    {
+        try {
+            File dftfile = new File(Environment.getExternalStorageDirectory().toString()+"/cos.txt");
+
+            if(!dftfile.exists())
+            {
+                dftfile.createNewFile();
+
+            }
+            FileWriter fw =new FileWriter(dftfile,true);
+            //fw.append(text);
+            //fw.append(text1);
+            fw.append("=======\n");
+
+            fw.append(s2);
+            fw.flush();
+            fw.close();
+            Toast.makeText(context1, "cos calculated", Toast.LENGTH_SHORT).show();
+
+
+        } catch (Exception e) {
+            //Toast.makeText(getBaseContext(), e.getMessage(),
+            // Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
 
 
